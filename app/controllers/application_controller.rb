dbd4872d7482f8c5e0eedf496d588e002d5f82b9
Swetha Srinivasan <swethavasan97@gmail.com>
2022-03-08 13:49:51 +0530

    Complete authetication system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
+  
+
   protect_from_forgery with: :exception
+
+  helper_method :current_chef, :logged_in?
 
   def current_chef
     @current_chef ||= Chef.find(session[:chef_id]) if session[:chef_id]
