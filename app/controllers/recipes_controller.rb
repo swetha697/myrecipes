b11841f3f67eaf5442229f9456685b9d8686607a
Swetha Srinivasan <swethavasan97@gmail.com>
2022-03-09 15:11:22 +0530

    Complete ingredients resource

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -53,12 +53,12 @@ class RecipesController < ApplicationController
       	@recipe = Recipe.find(params[:id])
       end
 
-	  def recipe_params
-	  	params.require(:recipe).permit(:name, :description)
-	  end
+	def recipe_params
+  		params.require(:recipe).permit(:name, :description, ingredient_ids: [])
+	end
 
 	  def require_same_user
-	      if current_chef != @recipe.chef
+	      if current_chef != @recipe.chef and !current_chef.admin?
 	        flash[:danger] = "You can only edit or delete your own recipes"
 	        redirect_to recipes_path
 	      end  
